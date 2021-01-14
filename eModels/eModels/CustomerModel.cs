ebf9f72728ba605455e749cd3743401b19345fba
Leap Outdom <leapoutdom@gmail.com>
2021-01-14 17:32:03 +0700

    update

---
+++ eModels/eModels/CustomerModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eModels
+{
+    [Table("tbl_customer")]
+    public class CustomerModel : CoreModel
+    {
+        [Required(ErrorMessage = "Please select a customer group.")]
+        public Guid customer_group_id { get; set; }
+        [ForeignKey("customer_group_id")]
+        public CustomerGroupModel customer_group { get; set; }
+
+
+
+    }
+}
---
+++ eModels/eModels/SettingModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;     
+
+namespace eModels
+{
+    [Table("tbl_setting")]
+    public  class SettingModel
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Key]
+        public int id { get; set; }
+
+        public string setting_value { get; set; }
+        public string setting_title { get; set; }
+        public string setting_description { get; set; }
+        public bool status { get; set; }
+    }
+}
