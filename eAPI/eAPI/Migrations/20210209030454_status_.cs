1f6ec416e4ce488ecfe0685ceb3bac7ec15e6f15
KimHimley <65536251+KimHimley@users.noreply.github.com>
2021-02-09 11:22:37 +0700

    f

---
+++ eAPI/eAPI/Migrations/20210209030454_status_.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eAPI.Migrations
+{
+    public partial class status_ : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "status",
+                table: "tbl_business_branch_price_rule",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "status",
+                table: "tbl_business_branch_price_rule");
+        }
+    }
+}
