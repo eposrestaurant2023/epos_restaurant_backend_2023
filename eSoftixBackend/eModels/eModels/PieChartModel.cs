8915221b32d3c9c0175e63ff13431ba13921d6f9
MrHo-IT <MrHo-IT@DESKTOP-CH4HBT4>
2021-02-18 17:39:47 +0700

    update

---
+++ eSoftixBackend/eModels/eModels/PieChartModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eModels
+{
+    public class PieChartModel
+    {
+        public string label { get; set; }
+        public string color { get; set; }
+        public double value { get; set; }
+    }
+}
