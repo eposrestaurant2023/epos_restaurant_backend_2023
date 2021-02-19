403cacd8197a7f62c3a71e0e50d72a647f478ed4
MrHo-IT <MrHo-IT@DESKTOP-CH4HBT4>
2021-02-19 11:25:04 +0700

    update

---
+++ eSoftixBackend/eModels/eModels/ContactModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace eModels
+{
+    public class ContactModel:CoreModel
+    {
+        public int customer_id { get; set; }
+        [ForeignKey("customer_id")]
+        public CustomerModel customer { get; set; }
+
+        public string contact_name { get; set; }
+        public string phone_1 { get; set; }
+        public string phone_2 { get; set; }
+        public string postion { get; set; }
+        public string gender { get; set; }
+        public string email_address { get; set; }
+    }
+}
