2eaf0fafdb9c1d07439d89812cf9fe8e88eeb7ed
KimHimley <65536251+KimHimley@users.noreply.github.com>
2021-02-25 10:41:13 +0700

    update

---
+++ eAPI/eAPI/Migrations/20210225033744_sort_order_modifier_group_item.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eAPI.Migrations
+{
+    public partial class sort_order_modifier_group_item : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "sort_order",
+                table: "tbl_modifier");
+
+            migrationBuilder.AddColumn<int>(
+                name: "sort_order",
+                table: "tbl_modifier_group_item",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "sort_order",
+                table: "tbl_modifier_group_item");
+
+            migrationBuilder.AddColumn<int>(
+                name: "sort_order",
+                table: "tbl_modifier",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
