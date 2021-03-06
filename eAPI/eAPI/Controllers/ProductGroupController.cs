3d4707ed54feb664a0c7bbb4a16cf8d3579d4086
veasnaesoftix <65535819+veasnaesoftix@users.noreply.github.com>
2021-03-06 13:52:43 +0700

    xx

--- eAPI/eAPI/Controllers/ProductGroupController.cs
+++ eAPI/eAPI/Controllers/ProductGroupController.cs
@@ -60,12 +60,11 @@ namespace eAPI.Controllers
 
 
         [HttpPost]
-        [Route("delete/{id}")]
-        public async Task<ActionResult<ProductGroupModel>> DeleteRecord(int id) //Delete
-        {
-            var u = await db.ProductGroups.FindAsync(id);
+        [Route("delete")]
+        public async Task<ActionResult<ProductGroupModel>> DeleteRecord(ProductGroupModel u) //Delete
+        { 
             u.is_deleted = !u.is_deleted;
-
+            u.product_categories.ForEach(r => r.is_deleted = u.is_deleted);
             db.ProductGroups.Update(u);
             await db.SaveChangesAsync();
             return Ok(u);
