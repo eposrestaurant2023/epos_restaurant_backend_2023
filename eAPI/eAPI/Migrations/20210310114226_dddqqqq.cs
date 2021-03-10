8e104124e6d460f178f0f620cabb9936f2090464
KimHimley <65536251+KimHimley@users.noreply.github.com>
2021-03-10 19:37:08 +0700

    update

---
+++ eAPI/eAPI/Migrations/20210310114226_dddqqqq.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eAPI.Migrations
+{
+    public partial class dddqqqq : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "setting_option",
+                table: "tbl_setting",
+                type: "nvarchar(max)",
+                nullable: true,
+                collation: "Khmer_100_BIN");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "setting_option",
+                table: "tbl_setting");
+        }
+    }
+}
