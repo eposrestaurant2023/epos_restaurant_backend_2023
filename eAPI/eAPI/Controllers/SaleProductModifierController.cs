44994aeb61242e5776fe0ef3d43e2ef97df66db7
KimHimley <65536251+KimHimley@users.noreply.github.com>
2021-03-12 17:15:17 +0700

    fix

---
+++ eAPI/eAPI/Controllers/SaleProductModifierController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+using eAPI.Services;
+using eModels;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;   
+using NETCore.Encrypt;
+using System.Text.Json;
+
+namespace eAPI.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SaleProductModifierController : ODataController
+    {
+        private readonly ApplicationDbContext db;
+        public SaleProductModifierController(ApplicationDbContext _db)
+        {
+            db = _db;
+        }        
+
+        [HttpGet]
+        [EnableQuery(MaxExpansionDepth = 8)]   
+        public IQueryable<SaleProductModifierModel> Get(string keyword = "")
+        {
+            if (!string.IsNullOrEmpty(keyword))
+            {
+              return (from r in db.SaleProductModifiers
+                           where 
+                                 EF.Functions.Like(
+                                     (
+                                        (r.sale_product.sale.document_number ?? " ") + 
+                                        (r.sale_product.product.product_display_name ?? " ") +
+                                        (r.sale_product.product.product_code ?? " ") +
+                                        (r.sale_product.sale.business_branch.business_branch_name_kh ?? " ") + 
+                                        (r.sale_product.sale.business_branch.business_branch_name_en ?? " ") + 
+                                        (r.sale_product.sale.sale_note ?? " ") 
+                                     ).ToLower().Trim(), $"%{keyword}%".ToLower().Trim())
+                           select r);
+            }
+            else
+            {
+                return db.SaleProductModifiers.AsQueryable();
+            }
+        }
+
+        [HttpGet]
+        [EnableQuery(MaxExpansionDepth = 8)]
+        [Route("getsingle")]
+        public async Task<SingleResult<SaleProductModifierModel>> Get([FromODataUri] Guid key)
+        {
+            return await Task.Factory.StartNew(() => SingleResult.Create<SaleProductModifierModel>(db.SaleProductModifiers.Where(r => r.id == key).AsQueryable()));
+        }
+    }
+}
