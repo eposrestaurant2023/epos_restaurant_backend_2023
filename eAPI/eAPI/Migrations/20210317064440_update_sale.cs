7b71ebc2d4948fe73df143367d0bca973e1a415d
veasnaesoftix <65535819+veasnaesoftix@users.noreply.github.com>
2021-03-17 13:50:30 +0700

    x

---
+++ eAPI/eAPI/Migrations/20210317064440_update_sale.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eAPI.Migrations
+{
+    public partial class update_sale : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "discount_code",
+                table: "tbl_sale",
+                type: "nvarchar(max)",
+                nullable: true,
+                collation: "Khmer_100_BIN");
+
+            migrationBuilder.AddColumn<string>(
+                name: "discount_note",
+                table: "tbl_sale",
+                type: "nvarchar(max)",
+                nullable: true,
+                collation: "Khmer_100_BIN");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "discount_code",
+                table: "tbl_sale");
+
+            migrationBuilder.DropColumn(
+                name: "discount_note",
+                table: "tbl_sale");
+        }
+    }
+}
