520ee5cb424582f5a86fd6f06aa4fe186cf9b435
tespheakdeyesoftix <pheakdey.micronet@gmail.com>
2021-03-17 17:40:24 +0700

    fix

--- eSoftixBackend/eModels/eModels/BusinessBranchModel.cs
+++ eSoftixBackend/eModels/eModels/BusinessBranchModel.cs
@@ -6,59 +6,26 @@ using eShareModel;
 namespace eModels
 {
     [Table("tbl_business_branch")]
-    public class BusinessBranchModel   : CoreGUIDModel
+    public class BusinessBranchModel   : eShareModel.BusinessBranchModel
     {
         public BusinessBranchModel()
         {
             outlets = new List<OutletModel>();
-            stations = new List<StationModel>();
+            stock_locations = new List<StockLocationModel>();
+            contacts = new List<ContactModel>();
         }
 
-        private string _business_branch_name_en;
-
-        [Required(ErrorMessage = "Field cannot be blank.")]
-        [MaxLength(100)]
-        public string business_branch_name_en
-        {
-            get { return _business_branch_name_en; }
-            set { 
-                _business_branch_name_en = value;
-                if(string.IsNullOrEmpty(business_branch_name_kh))
-                {
-                    business_branch_name_kh = value;
-                }
-            }
-        }
-
+        
         public  Guid project_id { get; set; }
         [ForeignKey("project_id")]
         public ProjectModel Project { get; set; }
-
-        [MaxLength(100)]
-        public string business_branch_name_kh { get; set; }
-
-        public string address_en { get; set; }
-        public string address_kh { get; set; }
-
-        [MaxLength(50)]
-        public string email { get; set; }
-
-        [MaxLength(50)]
-        public string phone_1 { get; set; }
-
-        [MaxLength(50)]
-        public string phone_2 { get; set; }
-
-        [MaxLength(50)]
-        public string website { get; set; }
-          
-        public string other { get; set; }
-
-        public string logo { get; set; }
+         
         public List<OutletModel>  outlets { get; set; }
-        public List<StationModel>  stations { get; set; }
 
-                                                                                       
+
+        public List<StockLocationModel> stock_locations { get; set; }
+
+        public List<ContactModel> contacts { get; set; }
 
     }
 }
