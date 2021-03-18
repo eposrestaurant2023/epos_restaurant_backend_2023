76185e45e77d06a9d5f0206737ea4b762eb3737b
KimHimley <65536251+KimHimley@users.noreply.github.com>
2021-03-18 16:31:32 +0700

    f

--- eAPI/eAPI/Migrations/20210318085939_ddd.Designer.cs
+++ eAPI/eAPI/Migrations/20210318085939_ddd.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eAPI;
 
 namespace eAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210318085939_ddd")]
+    partial class ddd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -977,9 +979,6 @@ namespace eAPI.Migrations
                     b.Property<string>("reference_number")
                         .HasColumnType("nvarchar(max)")
                         .UseCollation("Khmer_100_BIN");
-
-                    b.Property<Guid?>("sale_id")
-                        .HasColumnType("uniqueidentifier");
 
                     b.Property<Guid>("stock_location_id")
                         .HasColumnType("uniqueidentifier");
