04a7c303bd4daa625ccb2b052be0bc1550c3362c
tespheakdeyesoftix <pheakdey.micronet@gmail.com>
2021-06-22 15:24:48 +0700

    Update AppState.razor.cs

--- eSoftixBackend/eAdmin/eAdmin/AppState.razor.cs
+++ eSoftixBackend/eAdmin/eAdmin/AppState.razor.cs
@@ -93,8 +93,31 @@ namespace eAdmin
         {Palette = new Palette()
         {Primary = Colors.Blue.Default, Secondary = Colors.Green.Accent4, AppbarBackground = Colors.Red.Default, }, };
         public MudTheme darkTheme = new MudTheme()
-        {Palette = new Palette()
-        {Black = "#27272f", Background = "#32333d", BackgroundGrey = "#27272f", Surface = "#373740", DrawerBackground = "#27272f", DrawerText = "rgba(255,255,255, 0.50)", DrawerIcon = "rgba(255,255,255, 0.50)", AppbarBackground = "#27272f", AppbarText = "rgba(255,255,255, 0.70)", TextPrimary = "rgba(255,255,255, 0.70)", TextSecondary = "rgba(255,255,255, 0.50)", ActionDefault = "#adadb1", ActionDisabled = "rgba(255,255,255, 0.26)", ActionDisabledBackground = "rgba(255,255,255, 0.12)", Divider = "rgba(255,255,255, 0.12)", DividerLight = "rgba(255,255,255, 0.06)", TableLines = "rgba(255,255,255, 0.12)", LinesDefault = "rgba(255,255,255, 0.12)", LinesInputs = "rgba(255,255,255, 0.3)", TextDisabled = "rgba(255,255,255, 0.2)"}};
+        {
+            Palette = new Palette()
+            {
+                Black = "#27272f",
+                Background = "#32333d",
+                BackgroundGrey = "#27272f",
+                Surface = "#373740",
+                DrawerBackground = "#27272f",
+                DrawerText = "rgba(255,255,255, 0.50)",
+                DrawerIcon = "rgba(255,255,255, 0.50)",
+                AppbarBackground = "#27272f",
+                AppbarText = "rgba(255,255,255, 0.70)",
+                TextPrimary = "rgba(255,255,255, 0.70)",
+                TextSecondary = "rgba(255,255,255, 0.50)",
+                ActionDefault = "#adadb1",
+                ActionDisabled = "rgba(255,255,255, 0.26)",
+                ActionDisabledBackground = "rgba(255,255,255, 0.12)",
+                Divider = "rgba(255,255,255, 0.12)",
+                DividerLight = "rgba(255,255,255, 0.06)",
+                TableLines = "rgba(255,255,255, 0.12)",
+                LinesDefault = "rgba(255,255,255, 0.12)",
+                LinesInputs = "rgba(255,255,255, 0.3)",
+                TextDisabled = "rgba(255,255,255, 0.2)"
+            }
+        };
 
         protected override async Task OnInitializedAsync()
         {
