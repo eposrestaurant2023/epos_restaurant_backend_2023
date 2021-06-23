c49f52c38c5c0a74fb5a95b2fd939ef2b5716cf0
veasnaesoftix <65535819+veasnaesoftix@users.noreply.github.com>
2021-06-23 16:35:31 +0700

    zx

---
+++ eAPI/eAPI/Migrations/20210623093502_update_add_sale_order_id.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eAPI.Migrations
+{
+    public partial class update_add_sale_order_id : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "sale_order_id",
+                table: "tbl_sale_product",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "sale_order_id",
+                table: "tbl_sale_product");
+        }
+    }
+}
