1408c68018f8c4a509e81cba33c862fd2ac5dabc
veasnaesoftix <65535819+veasnaesoftix@users.noreply.github.com>
2021-07-01 17:51:33 +0700

    x

---
+++ eAdmin/eAdmin/Pages/PageSettings/PageTaxSettings/PageTaxSetting.razor.cs
@@ -0,0 +1,16 @@
+﻿using eAdmin.JSHelpers;
+using eModels;
+using MatBlazor;
+using Microsoft.AspNetCore.Components;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+
+namespace eAdmin.Pages.PageSettings.PageTaxSettings
+{
+    public class PageTaxSettingCore : PageCore
+    {
+        //
+    }
+}
