09167752b35138dd7d8ebe1a4403c793aed240c8
veasnaesoftix <65535819+veasnaesoftix@users.noreply.github.com>
2021-07-02 09:58:57 +0700

    xx

--- eAdmin/eAdmin/Pages/PageSettings/PageTaxSettings/PageTaxSetting.razor.cs
+++ eAdmin/eAdmin/Pages/PageSettings/PageTaxSettings/PageTaxSetting.razor.cs
@@ -11,6 +11,13 @@ namespace eAdmin.Pages.PageSettings.PageTaxSettings
 {
     public class PageTaxSettingCore : PageCore
     {
-        //
+        public eShareModel.TaxRuleModel tax_rule = new eShareModel.TaxRuleModel();
+        protected override async Task OnInitializedAsync()
+        {
+            is_loading = true;
+            tax_rule = JsonSerializer.Deserialize<eShareModel.TaxRuleModel>(gv.tax_rule);
+            is_loading = false;
+        }
+
     }
 }
--- eShareModel/eShareModel/TaxRuleModel.cs
+++ eShareModel/eShareModel/TaxRuleModel.cs
@@ -13,15 +13,15 @@ namespace eShareModel
         public string tax_2_name { get; set; } = "P/L Tax";
         public string tax_3_name { get; set; } = "VAT";
 
-        public bool calc_tax_1_after_adding_discount { get; set; } = true;
+        public bool calc_tax_1_after_discount { get; set; } = true;
 
-        public bool calc_tax_2_after_adding_discount { get; set; } = true;
+        public bool calc_tax_2_after_discount { get; set; } = true;
         public bool calc_tax_2_after_adding_tax_1 { get; set; } = true;
         public bool calc_tax_2_taxable_rate_after_adding_tax_1 { get; set; } = true;
 
         public bool calc_tax_3_taxable_rate_after_adding_tax_1 { get; set; } = false;
         public bool calc_tax_3_taxable_rate_after_adding_tax_2 { get; set; } = false;
-        public bool calc_tax_3_after_adding_discount { get; set; } = true;
+        public bool calc_tax_3_after_discount { get; set; } = true;
         public bool calc_tax_3_after_adding_tax_1 { get; set; } = true;
         public bool calc_tax_3_after_adding_tax_2 { get; set; } = true;
         
