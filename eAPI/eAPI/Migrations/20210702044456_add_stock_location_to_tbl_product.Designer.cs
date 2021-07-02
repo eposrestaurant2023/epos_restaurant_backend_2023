b10b2b2d23302ddf4f7f4cba16aa9993b9b10a63
tespheakdeyesoftix <pheakdey.micronet@gmail.com>
2021-07-02 11:56:08 +0700

    fix product stock location

--- eAPI/eAPI/Migrations/20210702044456_add_stock_location_to_tbl_product.Designer.cs
+++ eAPI/eAPI/Migrations/20210702044456_add_stock_location_to_tbl_product.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eAPI;
 
 namespace eAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210702044456_add_stock_location_to_tbl_product")]
+    partial class add_stock_location_to_tbl_product
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
