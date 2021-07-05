90af0c42841bb7732ec699bf3e81a2b4f8adf5d8
tespheakdeyesoftix <pheakdey.micronet@gmail.com>
2021-07-05 06:39:13 -0700

    update

--- eSoftixBackend/eAPI/eAPI/Migrations/20210703195144_xx.Designer.cs
+++ eSoftixBackend/eAPI/eAPI/Migrations/20210703195144_xx.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eAPI;
 
 namespace eAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210703195144_xx")]
+    partial class xx
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ eSoftixBackend/eAPI/eAPI/Migrations/20210703195144_xx.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eAPI.Migrations
+{
+    public partial class xx : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "stock_location_name",
+                table: "tbl_stock_location",
+                type: "nvarchar(max)",
+                nullable: true,
+                collation: "Khmer_100_BIN",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldCollation: "Khmer_100_BIN");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "contact_name",
+                table: "tbl_business_branch",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                collation: "Khmer_100_BIN",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true,
+                oldCollation: "Khmer_100_BIN");
+        }
+    }
+}
