cf54ba7f2522a0d177f0fd4ab8928651d1d3e837
tespheakdeyesoftix <pheakdey.micronet@gmail.com>
2021-07-05 10:27:44 +0700

    fix

--- eAPI/eAPI/Migrations/20210705032628_add_contact_name_to_business_ranch.Designer.cs
+++ eAPI/eAPI/Migrations/20210705032628_add_contact_name_to_business_ranch.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eAPI;
 
 namespace eAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210705032628_add_contact_name_to_business_ranch")]
+    partial class add_contact_name_to_business_ranch
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
