533f60f439cacef1068089ab12ecc2980feeea88
D O M Y <outdom.leap@esoftix.com>
2021-07-07 14:01:55 +0700

    update

--- eAPI/eAPI/Migrations/20210707070123_is_print.Designer.cs
+++ eAPI/eAPI/Migrations/20210707070123_is_print.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eAPI;
 
 namespace eAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210707070123_is_print")]
+    partial class is_print
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
