65f5f7cd9acea03cca920f9327efc8da838bb312
D O M Y <outdom.leap@esoftix.com>
2021-07-09 15:55:43 +0700

    udate

--- eAPIClient/eAPIClient/Migrations/20210709085516_sale_proeuct_moid.Designer.cs
+++ eAPIClient/eAPIClient/Migrations/20210709085516_sale_proeuct_moid.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eAPIClient;
 
 namespace eAPIClient.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210709085516_sale_proeuct_moid")]
+    partial class sale_proeuct_moid
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
