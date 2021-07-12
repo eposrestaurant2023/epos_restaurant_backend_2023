0ae9a753f56b9828c434bd8828cfc1369dc8b907
D O M Y <outdom.leap@esoftix.com>
2021-07-12 15:11:47 +0700

    update

--- eAPI/eAPI/Migrations/20210712071426_dddswxx.Designer.cs
+++ eAPI/eAPI/Migrations/20210712071426_dddswxx.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eAPI;
 
 namespace eAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210712071426_dddswxx")]
+    partial class dddswxx
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -1816,11 +1818,15 @@ namespace eAPI.Migrations
                         .HasColumnType("int")
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
+                    b.Property<int>("currency_id")
+                        .HasColumnType("int");
+
                     b.Property<int>("payment_type_id")
                         .HasColumnType("int");
 
-                    b.Property<decimal>("predefine_value")
-                        .HasColumnType("decimal(19,4)");
+                    b.Property<string>("predefine_value")
+                        .HasColumnType("nvarchar(max)")
+                        .UseCollation("Khmer_100_BIN");
 
                     b.Property<int>("sort_order")
                         .HasColumnType("int");
--- eModels/eModels/PredefinePaymentAmountModel.cs
+++ eModels/eModels/PredefinePaymentAmountModel.cs
@@ -5,14 +5,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eModels
 {
-    [Table("tbl_predefined_payment_amount")]
+    [Table("tbl_predefine_payment_amount")]
     public class PredefinePaymentAmountModel
     {
         [Key]
         public int id { get; set; }
-        public string predefine_value { get; set; }
-        public int payment_type_id { get; set; }
-        public int currency_id { get; set; }
+        public decimal predefine_value { get; set; }
+        public int payment_type_id { get; set; } 
         public int sort_order { get; set; }
         public bool status { get; set; }
     }
