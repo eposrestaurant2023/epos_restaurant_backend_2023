f950c288039e033a3c49eb4d890bb5b4b5bbeb47
D O M Y <outdom.leap@esoftix.com>
2021-07-12 14:13:51 +0700

    update

--- eModels/eModels/PredefinePaymentAmountModel.cs
+++ eModels/eModels/PredefinePaymentAmountModel.cs
@@ -6,11 +6,11 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace eModels
 {
     [Table("tbl_predefined_payment_amount")]
-    public class PredefinedPaymentAmountModel
+    public class PredefinePaymentAmountModel
     {
-        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Key]
         public int id { get; set; }
-        public string prefix_price_value { get; set; }
+        public string predefine_value { get; set; }
         public int payment_type_id { get; set; }
         public int currency_id { get; set; }
         public int sort_order { get; set; }
