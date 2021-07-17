21d240d56743984b4708b03c88d02153041a8594
MrHo-IT <65536015+sengho777@users.noreply.github.com>
2021-07-17 17:31:34 +0700

    update

--- eAPI/eAPI/Controllers/SaleController.cs
+++ eAPI/eAPI/Controllers/SaleController.cs
@@ -72,6 +72,7 @@ namespace eAPI.Controllers
                 var _chk = db.Sales.Where(r => r.id == model.id).AsNoTracking();
                 if (_chk.Count() > 0)
                 {
+                     
                     model.sale_products.ForEach(_sp =>
                     {
                         if (db.SaleProducts.Where(r => r.id == _sp.id).Count() <= 0)
@@ -94,7 +95,7 @@ namespace eAPI.Controllers
 
                     model.sale_payments.ForEach(_spay =>
                     {
-                        if (db.SalePayments.Where(r => r.id == _spay.id).Count() <= 0)
+                        if (db.SalePayments.Where(r => r.id == _spay.id).AsNoTracking().Count() <= 0)
                         {
                             db.SalePayments.Add(_spay);
                             db.SaveChanges();
