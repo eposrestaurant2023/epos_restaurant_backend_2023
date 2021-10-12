ac36df5acd2e0268c38e94abd8c7056375161f7b
chanthoudein <65535903+chanthoudein@users.noreply.github.com>
2021-10-12 15:42:30 +0700

    f

--- eAdmin/eAdmin/Pages/PageReports/PageReport.razor.cs
+++ eAdmin/eAdmin/Pages/PageReports/PageReport.razor.cs
@@ -10,8 +10,24 @@ namespace eAdmin.Pages.PageReports
     {
         public bool IsOpenedReport { get; set; } = false;
         public List<PermissionOptionModel> main_report_list = new List<PermissionOptionModel>();
-
+        public string keyword { get; set; } = "";
         public PermissionOptionModel report { get; set; } = new PermissionOptionModel();
+
+        public string report_title
+        {
+            get
+            {
+                if (report != null && report.id > 0)
+                {
+                    return "Report - " + report.report_title;
+                }
+                else
+                {
+                    return "Report";
+                }
+            }
+
+        }
         protected override async Task OnInitializedAsync()
         {
 
@@ -20,10 +36,20 @@ namespace eAdmin.Pages.PageReports
             main_report_list = gv.permission_options.Where(r => r.parent_id == 44 && r.is_public_report == true).ToList();
             var x = main_report_list;
         }
-        public void ViewReportClick(PermissionOptionModel _report)
+
+        public async Task ViewReportClick(PermissionOptionModel _report)
         {
-            report = _report;
-            IsOpenedReport = true;
+            if (report == null) report = new PermissionOptionModel();
+            if (report.id != _report.id)
+            {
+                is_loading_data = true;
+                await Task.Delay(100);
+                report = _report;
+                is_loading_data = false;
+            }
+
+
+
         }
     }
 }
