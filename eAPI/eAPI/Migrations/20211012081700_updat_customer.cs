b635af120fe0daa74e6537dc0b28e994563931f9
tespheakdeyesoftix <pheakdey.micronet@gmail.com>
2021-10-12 15:37:39 +0700

    fix

---
+++ eAPI/eAPI/Migrations/20211012081700_updat_customer.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eAPI.Migrations
+{
+    public partial class updat_customer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "customer_code",
+                table: "tbl_sale",
+                type: "nvarchar(max)",
+                nullable: true,
+                collation: "Khmer_100_BIN");
+
+            migrationBuilder.AddColumn<string>(
+                name: "customer_name",
+                table: "tbl_sale",
+                type: "nvarchar(max)",
+                nullable: true,
+                collation: "Khmer_100_BIN");
+
+            migrationBuilder.AddColumn<string>(
+                name: "phone_number",
+                table: "tbl_sale",
+                type: "nvarchar(max)",
+                nullable: true,
+                collation: "Khmer_100_BIN");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "customer_code",
+                table: "tbl_sale");
+
+            migrationBuilder.DropColumn(
+                name: "customer_name",
+                table: "tbl_sale");
+
+            migrationBuilder.DropColumn(
+                name: "phone_number",
+                table: "tbl_sale");
+        }
+    }
+}
