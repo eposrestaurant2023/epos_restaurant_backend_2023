c72cf6736df36bae729e3c12963e97bfd63b4c96
kimchhaychheng555 <72429607+kimchhaychheng555@users.noreply.github.com>
2021-10-26 17:38:49 +0700

    update

---
+++ eAPIClient/eAPIClient/Migrations/20211026103151_add_business_branch_name_to_working_day_model.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eAPIClient.Migrations
+{
+    public partial class add_business_branch_name_to_working_day_model : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "business_branch_name_en",
+                table: "tbl_working_day",
+                type: "nvarchar(max)",
+                nullable: true,
+                collation: "Khmer_100_BIN");
+
+            migrationBuilder.AddColumn<string>(
+                name: "business_branch_name_kh",
+                table: "tbl_working_day",
+                type: "nvarchar(max)",
+                nullable: true,
+                collation: "Khmer_100_BIN");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "business_branch_name_en",
+                table: "tbl_working_day");
+
+            migrationBuilder.DropColumn(
+                name: "business_branch_name_kh",
+                table: "tbl_working_day");
+        }
+    }
+}
