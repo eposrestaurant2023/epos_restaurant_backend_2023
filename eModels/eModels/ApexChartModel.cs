12ee4d44eea70ddef886f50585349fdfba8093be
sengho777 <65536015+sengho777@users.noreply.github.com>
2021-10-30 17:36:51 +0700

    update

---
+++ eModels/eModels/ApexChartModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eModels
+{
+    public class ApexChartModel
+    {
+        public ApexChartModel()
+        {
+            data = new List<ApexChartDataModel>();
+        }
+        public string title { get; set; }
+        public string color { get; set; }
+
+        public List<ApexChartDataModel> data { get; set; } = new List<ApexChartDataModel>();
+    }
+    public class ApexChartDataModel
+    {
+        public string label { get; set; }
+        public decimal value { get; set; }
+    }
+}
