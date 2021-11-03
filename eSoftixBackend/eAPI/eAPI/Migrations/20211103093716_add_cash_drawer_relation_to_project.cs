5763b23cd542327ba8ce0420fb81101de8d5a9b3
chanthoudein <65535903+chanthoudein@users.noreply.github.com>
2021-11-03 16:38:22 +0700

    d

---
+++ eSoftixBackend/eAPI/eAPI/Migrations/20211103093716_add_cash_drawer_relation_to_project.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eAPI.Migrations
+{
+    public partial class add_cash_drawer_relation_to_project : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
