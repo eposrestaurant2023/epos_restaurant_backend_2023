6ecf23861dd9353b92f018d3b96573dfe10836f6
sengho777 <65536015+sengho777@users.noreply.github.com>
2021-11-06 10:09:24 +0700

    update

--- eModels/eModels/PurchaseOrderPaymentModel.cs
+++ eModels/eModels/PurchaseOrderPaymentModel.cs
@@ -39,6 +39,7 @@ namespace eModels
         public string currency_name_en { get; set; }
         public string currency_name_kh { get; set; }
         public string currency_format { get; set; }
+        public double change_exchange_rate { get; set; }
 
         [NotMapped, JsonIgnore]
         public bool is_add_note { get; set; }
