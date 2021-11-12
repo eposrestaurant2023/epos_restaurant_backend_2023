053586816c3c798b8c7a5a81c7a8c4fac95a7ee8
tespheakdeyesoftix <pheakdey.micronet@gmail.com>
2021-11-12 10:10:44 +0700

    fix currency

---
+++ eAPI/eAPI/Migrations/20211112025445_add_deleted_to_cuirrency_4.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eAPI.Migrations
+{
+    public partial class add_deleted_to_cuirrency_4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_tbl_business_branch_currency",
+                table: "tbl_business_branch_currency",
+                column: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_tbl_business_branch_currency",
+                table: "tbl_business_branch_currency");
+        }
+    }
+}
--- eModels/eModels/CurrencyModel.cs
+++ eModels/eModels/CurrencyModel.cs
@@ -1,4 +1,5 @@
-﻿using eShareModel;                                    
+﻿using eShareModel;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;    
 
 namespace eModels
@@ -6,7 +7,18 @@ namespace eModels
     [Table("tbl_currency")]
     public class CurrencyModel    : CurrencyShareModel
     {
+        public CurrencyModel()
+        {
+           business_branch_currencies = new List<BusinessBranchCurrencyModel>();
+        }
         public double default_exchange_rate { get; set; }
         public double default_change_exchange_rate { get; set; }
+
+     public List<BusinessBranchCurrencyModel> business_branch_currencies { get; set; }
+
+        public bool is_deleted { get; set; }
+      
+
+
     }
 }
