a938327dc4211c75adf74debddfbd02e9f7bb340
tespheakdeyesoftix <pheakdey.micronet@gmail.com>
2021-11-24 17:04:27 +0700

    update history client

---
+++ eAPI/eAPI/Migrations/20211124100112_update_historyt_10.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eAPI.Migrations
+{
+    public partial class update_historyt_10 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "business_branch_name",
+                table: "tbl_history",
+                type: "nvarchar(max)",
+                nullable: true,
+                collation: "Khmer_100_BIN");
+
+            migrationBuilder.AddColumn<string>(
+                name: "outlet_name",
+                table: "tbl_history",
+                type: "nvarchar(max)",
+                nullable: true,
+                collation: "Khmer_100_BIN");
+
+            migrationBuilder.AddColumn<string>(
+                name: "station_name",
+                table: "tbl_history",
+                type: "nvarchar(max)",
+                nullable: true,
+                collation: "Khmer_100_BIN");
+
+            migrationBuilder.AddColumn<string>(
+                name: "table_name",
+                table: "tbl_history",
+                type: "nvarchar(max)",
+                nullable: true,
+                collation: "Khmer_100_BIN");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "business_branch_name",
+                table: "tbl_history");
+
+            migrationBuilder.DropColumn(
+                name: "outlet_name",
+                table: "tbl_history");
+
+            migrationBuilder.DropColumn(
+                name: "station_name",
+                table: "tbl_history");
+
+            migrationBuilder.DropColumn(
+                name: "table_name",
+                table: "tbl_history");
+        }
+    }
+}
