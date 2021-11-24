881935f3733bbcc66383a84f4f8ee684c322d278
kimchhaychheng555 <72429607+kimchhaychheng555@users.noreply.github.com>
2021-11-24 17:38:12 +0700

    update

--- ePOSClientSyncService/ePOSClientSyncService/Program.cs
+++ ePOSClientSyncService/ePOSClientSyncService/Program.cs
@@ -2,15 +2,19 @@
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using RestSharp;
 
 namespace ePOSClientSyncService
 {
     class Program
     {
-      
+        static string DBConnection = "Data Source=192.168.10.13,1437;Initial Catalog=epos_restaurant_client_2021_db;Persist Security Info=True;User ID=sa;Password=admin@eSoftix";
+
         static void Main(string[] args)
         {
-            using var watcher = new FileSystemWatcher(@"C:\epossync\");
+            var watcher = new FileSystemWatcher(@"C:\epossync\");
 
             watcher.NotifyFilter = NotifyFilters.Attributes
                                  | NotifyFilters.CreationTime
@@ -21,10 +25,10 @@ namespace ePOSClientSyncService
                                  | NotifyFilters.Security
                                  | NotifyFilters.Size;
 
-         
-            watcher.Created +=OnCreatedAsync;
-          
-         
+
+            watcher.Created += OnCreatedAsync;
+
+
             watcher.Filter = "*.txt";
             watcher.IncludeSubdirectories = true;
             watcher.EnableRaisingEvents = true;
@@ -34,22 +38,46 @@ namespace ePOSClientSyncService
         }
 
 
-   
-       
+
+
         private static void OnCreatedAsync(object sender, FileSystemEventArgs e)
-        {
-               HttpClient client;
-            string value = e.Name.Replace(".txt","").Split(',')[0];
+        { 
+            string value = e.Name.Replace(".txt", "").Split(',')[0];
 
             try
             {
+                var client = new RestClient("http://localhost:7072/api/");
+              
 
-                HttpClientHandler clientHandler = new HttpClientHandler();
-                clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+                DataTable dt = ExecuteDataTable("exec sp_get_data_for_synchronize");
+                foreach(DataRow r in dt.Rows)
+                {
+                    string _query = "";
+                    switch (r["transaction_type"].ToString())
+                    {
+                        case "working_day":
+                             _query = $"Sync/SyncWorkingDay?workingDayId={r["id"].ToString()}";
+                            break;
+                        case "cash_drawer_amount":
+                             _query = $"Sync/SyncCashDrawerAmount?id={r["id"].ToString()}";
+                            break;
+                        case "cashier_shift":
+                            _query = $"Sync/SyncCashierShift?id={r["id"].ToString()}";
+                            break;
+                        case "sale":
+                             _query = $"Sync/SyncSale?saleId={r["id"].ToString()}";
+                            break;
+                        default:
+                            break;
+                    }
 
-                client = new HttpClient(clientHandler);
+                    if (!string.IsNullOrEmpty(_query))
+                    {
+                        var request = new RestRequest(_query);
+                        var response = client.Get(request);
+                    }
+                }
 
-                Task.Run(() =>   client.GetAsync($"http://localhost:7072/api/Sync/SyncSale?saleId={value}"));
 
 
                 File.Delete(e.FullPath);
@@ -59,14 +87,55 @@ namespace ePOSClientSyncService
             catch (Exception ex)
             {
 
-                
+
             }
 
             Console.WriteLine(value);
         }
 
 
-       
+
+        static DataTable ExecuteDataTable(string sql)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                using (SqlConnection con = new SqlConnection(DBConnection))
+                {
+                    if (con.State == ConnectionState.Closed)
+                        con.Open();
+                    SqlTransaction trans = con.BeginTransaction();
+                    SqlCommand cmd = new SqlCommand(sql, con, trans);
+                    try
+                    {
+                        SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        da.Fill(dt);
+                        trans.Commit();
+                        return dt;
+                    }
+                    catch (Exception ex)
+                    {
+
+                        trans.Rollback();
+                        //WriteToFile(ex.Message);
+                        return null;
+                    }
+                    finally
+                    {
+                        cmd.Dispose();
+                        trans.Dispose();
+                        if (con.State == ConnectionState.Open)
+                            con.Close();
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+
+                //WriteToFile(ex.Message);
+                return null;
+            }
+        }
 
     }
 }
