3a9ae425e0d7df686dd88927230d6083eb5d9fe4
tespheakdeyesoftix <pheakdey.micronet@gmail.com>
2021-11-30 14:46:25 +0700

    update

--- eAPI/eAPI/Migrations/20211130072535_add_local_setting_to_db_tbl_config_data.Designer.cs
+++ eAPI/eAPI/Migrations/20211130072535_add_local_setting_to_db_tbl_config_data.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eAPI;
 
 namespace eAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211130072535_add_local_setting_to_db_tbl_config_data")]
+    partial class add_local_setting_to_db_tbl_config_data
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
