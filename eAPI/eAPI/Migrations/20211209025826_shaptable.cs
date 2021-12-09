4ea38ee81fb8404a331134f0ecad0848093c2abf
chanthoudein <65535903+chanthoudein@users.noreply.github.com>
2021-12-09 10:00:34 +0700

    t

---
+++ eAPI/eAPI/Migrations/20211209025826_shaptable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eAPI.Migrations
+{
+    public partial class shaptable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "shape",
+                table: "tbl_table",
+                type: "nvarchar(max)",
+                nullable: true,
+                collation: "Khmer_100_BIN");
+
+            migrationBuilder.AddColumn<string>(
+                name: "customer_group_name",
+                table: "tbl_customer",
+                type: "nvarchar(max)",
+                nullable: true,
+                collation: "Khmer_100_BIN");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "shape",
+                table: "tbl_table");
+
+            migrationBuilder.DropColumn(
+                name: "customer_group_name",
+                table: "tbl_customer");
+        }
+    }
+}
