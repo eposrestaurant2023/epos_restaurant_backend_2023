02ffc4791dd7e01327b322c0a53103b4216abbca
tespheakdeyesoftix <pheakdey.micronet@gmail.com>
2022-02-10 14:43:47 +0700

    add knowledge base

---
+++ eKnowledgebase/eKnowledgebase/Models/eKnowledgebaseModel.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace eKnowledgebase.Models
+{
+    public class eKnowledgebaseModel
+    {
+         
+
+            public eKnowledgebaseModel()
+            {
+                children = new List<eKnowledgebaseModel>();
+            }
+
+            public eKnowledgebaseModel(Guid _id)
+            {
+                parent_id = _id;
+                children = new List<eKnowledgebaseModel>();
+            }
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+          
+            public Guid id { get; set; }
+
+            public string? icon { get; set; }
+
+            public string? title_en { get; set; }
+
+            public string? title_kh { get; set; }
+            public string? description_en { get; set; }
+            public string? description_kh { get; set; }
+            public string? photo { get; set; }
+
+            public bool is_deleted { get; set; } = false;
+
+            [NotMapped]
+            [JsonIgnore]
+            public bool is_select_parent { get; set; }
+            public Guid? parent_id { get; set; }
+            [ForeignKey("parent_id")]
+            public eKnowledgebaseModel? parent { get; set; }
+
+            public List<eKnowledgebaseModel> children { get; set; }
+
+            public int sort_order { get; set; }
+
+
+
+        
+    }
+}
---
+++ eKnowledgebase/eKnowledgebase/Program.cs
@@ -0,0 +1,37 @@
+using eKnowledgebase.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<HttpClient>(s =>
+{
+	// Creating the URI helper needs to wait until the JS Runtime is initialized, so defer it.
+	var uriHelper = s.GetRequiredService<NavigationManager>();
+	return new HttpClient
+	{
+		BaseAddress = new Uri(uriHelper.BaseUri)
+	};
+});
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddTransient<eKnowledgebase.Services.eKnowledgebaseService>();
+builder.Services.AddMudServices();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
