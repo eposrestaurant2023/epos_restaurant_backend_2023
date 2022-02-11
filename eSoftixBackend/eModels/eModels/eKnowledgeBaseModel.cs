8c3204c24483d3767b1a1fd10838a26dc9ff02e3
tespheakdeyesoftix <pheakdey.micronet@gmail.com>
2022-02-11 12:39:56 +0700

    update backend

--- eSoftixBackend/eModels/eModels/eKnowledgeBaseModel.cs
+++ eSoftixBackend/eModels/eModels/eKnowledgeBaseModel.cs
@@ -30,7 +30,7 @@ namespace eModels
         public string icon { get; set; }
         public bool is_public { get; set;}
 
-        public int short_order { get; set; }
+    
         public string title_en { get; set; }
         public string title_kh { get; set; }
         public string description_en { get; set; }
