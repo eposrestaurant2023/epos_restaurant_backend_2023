0c213597ec0d20e684660e4ea3a7b1e31ded9803
tespheakdeyesoftix <pheakdey.micronet@gmail.com>
2022-02-25 17:55:35 +0700

    Update AppController.cs

--- eAPI/eAPI/Controllers/AppController.cs
+++ eAPI/eAPI/Controllers/AppController.cs
@@ -289,7 +289,7 @@ namespace eAPI.Controllers
                 deviceId == "O0C30pohUFQEEbe3uJSIPOr4FKKne-zdIf4JBbwBA6Q" ||
                 deviceId == "Sc0UgR4gWFuepT5mHtZJZ8EQD1eMlNFgWFa4JVlHq60" ||
                 deviceId == "SDOoJUSChdeLkqSBYCcDhyCW_nuzlcnjFYacKAwQS9s" ||
-                deviceId == "CsUhcPGkd_hNOyjA1_7jk0t2mhjhWurAQ1MiCymzIj8" ||
+                deviceId == "cPTB7pEjQoCgGOpEgGLPxktvUmaICYQq7WVl1IzQPK8" ||
                 deviceId == "XJHGOXQ1QzMVlFTYrz2Cv7coEkEUmvHE-6hz9FFf8uk"
                     
 
