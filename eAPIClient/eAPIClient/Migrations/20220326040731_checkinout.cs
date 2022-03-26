d0e82c029c61357de34cdf3269c11623541abce3
D O M Y <outdom.leap@esoftix.com>
2022-03-26 11:08:14 +0700

    update

---
+++ eAPIClient/eAPIClient/Migrations/20220326040731_checkinout.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eAPIClient.Migrations
+{
+    public partial class checkinout : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "check_in_by",
+                table: "tbl_sale",
+                type: "nvarchar(max)",
+                nullable: true,
+                collation: "Khmer_100_BIN");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "check_in_date",
+                table: "tbl_sale",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "check_out_by",
+                table: "tbl_sale",
+                type: "nvarchar(max)",
+                nullable: true,
+                collation: "Khmer_100_BIN");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "check_out_date",
+                table: "tbl_sale",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "check_in_by",
+                table: "tbl_sale");
+
+            migrationBuilder.DropColumn(
+                name: "check_in_date",
+                table: "tbl_sale");
+
+            migrationBuilder.DropColumn(
+                name: "check_out_by",
+                table: "tbl_sale");
+
+            migrationBuilder.DropColumn(
+                name: "check_out_date",
+                table: "tbl_sale");
+        }
+    }
+}
