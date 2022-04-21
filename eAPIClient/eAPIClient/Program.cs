ed2d775d58afe2b3c9a46b66f96223582e9abe3a
D O M Y <outdom.leap@esoftix.com>
2022-04-21 15:40:38 +0700

    update

--- eAPIClient/eAPIClient/Program.cs
+++ eAPIClient/eAPIClient/Program.cs
@@ -1,31 +1,21 @@
 using eAPIClient.Services;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.SignalR.Client;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace eAPIClient
 {
     public static class Program
     {
-        public static async Task Main(string[] args)
+        public static void  Main(string[] args)
         {
-            
-
             var builder = CreateHostBuilder(args);
             var host = builder.Build();
 
 
-            var scope = host.Services.CreateScope();  
-            var services = scope.ServiceProvider;  
+            var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
             var sync = services.GetRequiredService<ISyncService>();
             var env = services.GetRequiredService<IWebHostEnvironment>();
             //string upload = env.ContentRootPath + "\\uploads";  
@@ -34,16 +24,13 @@ namespace eAPIClient
             //    Directory.CreateDirectory(upload);
             //}
 
-            string path = env.ContentRootPath + "\\logs";
-          
+            string path = env.ContentRootPath + "\\logs";      
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
             }
 
-            var watcher = new FileSystemWatcher(path);
-
-
+            var watcher = new FileSystemWatcher(path);  
             watcher.NotifyFilter = NotifyFilters.Attributes
                                  | NotifyFilters.CreationTime
                                  | NotifyFilters.DirectoryName
@@ -54,16 +41,12 @@ namespace eAPIClient
                                  | NotifyFilters.Size;
 
 
-            watcher.Created += sync.OnCreatedAsync;
-
-
+            watcher.Created += sync.OnCreatedAsync;  
             watcher.Filter = "*.txt";
             watcher.IncludeSubdirectories = true;
             watcher.EnableRaisingEvents = true;
 
-
-            //watch for sync remote data
-
+            //watch for sync remote data  
             var watcherRemoteData = new FileSystemWatcher(path);
 
             watcherRemoteData.NotifyFilter = NotifyFilters.Attributes
@@ -73,22 +56,14 @@ namespace eAPIClient
                                  | NotifyFilters.LastAccess
                                  | NotifyFilters.LastWrite
                                  | NotifyFilters.Security
-                                 | NotifyFilters.Size;
-
-
-            watcherRemoteData.Created += sync.OnSyncFromRemoteServerAsync;
-
-
+                                 | NotifyFilters.Size; 
+            watcherRemoteData.Created += sync.OnSyncFromRemoteServerAsync;  
             watcherRemoteData.Filter = "*.bat";
             watcherRemoteData.IncludeSubdirectories = true;
-            watcherRemoteData.EnableRaisingEvents = true;
-
-
-
-            host.Run();
-             
+            watcherRemoteData.EnableRaisingEvents = true;  
+            host.Run();     
         }
- 
+
 
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -96,9 +71,18 @@ namespace eAPIClient
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+            // .ConfigureAppConfiguration((hostingContext, config) =>
+            // {
+            //     var x = hostingContext;
+            //     var y = config;
+            // })
+            //.ConfigureWebHost(config =>
+            //    {
+                    
+            //        config.UseUrls("http://*:5050");
 
-
-
+            //    })
+            .UseWindowsService();
     }
 }
