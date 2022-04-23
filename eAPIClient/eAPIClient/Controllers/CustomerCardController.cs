7edea6613bb511d66a2d3c2863852250c1f29fa6
kimchhaychheng555 <72429607+kimchhaychheng555@users.noreply.github.com>
2022-04-23 15:48:48 +0700

    changes

---
+++ eAPIClient/eAPIClient/Controllers/CustomerCardController.cs
@@ -0,0 +1,48 @@
+﻿using eAPIClient.Models;
+using eAPIClient.Services;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eAPIClient.Controllers
+{
+    public class CustomerCardController : ODataController
+    {
+
+        private readonly ApplicationDbContext db;
+        private readonly AppService app;
+        private readonly ISyncService sync;
+
+        public CustomerCardController(ApplicationDbContext _db, AppService _app, ISyncService sync)
+        {
+            db = _db;
+            app = _app;
+            this.sync = sync;
+        }
+
+        [HttpGet]
+        [EnableQuery(MaxExpansionDepth = 0)]
+        [AllowAnonymous]
+        public IQueryable<CustomerCardModel> Get()
+        {
+
+
+            return db.CustomerCards;
+
+        }
+
+
+        [HttpGet]
+        [EnableQuery(MaxExpansionDepth = 0)]
+        [AllowAnonymous]
+        public SingleResult<CustomerCardModel> Get([FromODataUri] Guid key)
+        { 
+            var s = db.CustomerCards.Where(r => r.id == key).AsQueryable();
+            return SingleResult.Create(s); 
+        } 
+    }
+}
