1b85d695f52dfffba41c218004491cab86293ddd
tespheakdeyesoftix <pheakdey.micronet@gmail.com>
2022-05-25 10:12:43 +0700

    update

---
+++ eAPIClient/eAPIClient/Migrations/20220524101238_update_product_menu_add_sort_order.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eAPIClient.Migrations
+{
+    public partial class update_product_menu_add_sort_order : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "sort_order",
+                table: "tbl_product_menu",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "sort_order",
+                table: "tbl_product_menu");
+        }
+    }
+}
