761e88e21bcd1ba3ff1b57743d954729606558f6
kimchhaychheng555 <72429607+kimchhaychheng555@users.noreply.github.com>
2022-05-26 09:28:38 +0700

    changes

--- eAPIClient/eAPIClient/Controllers/SyncController.cs
+++ eAPIClient/eAPIClient/Controllers/SyncController.cs
@@ -556,7 +556,7 @@ namespace eAPIClient.Controllers
         async Task<List<MenuModel>> GetRemoteMenu(string business_branch_id)
         {      
             is_get_remote_data_success = false;
-            var resp = await http.ApiGetOData($"Menu?$select=id,parent_id,menu_name_en,menu_name_kh,text_color,background_color,root_menu_id,photo,is_shortcut_menu&$filter=business_branch_id eq {business_branch_id} and is_deleted eq false and status eq true");
+            var resp = await http.ApiGetOData($"Menu?$select=id,parent_id,menu_name_en,menu_name_kh,text_color,background_color,root_menu_id,photo,is_shortcut_menu,sort_order&$filter=business_branch_id eq {business_branch_id} and is_deleted eq false and status eq true");
             if (resp.IsSuccess)
             {
                 is_get_remote_data_success = true;
