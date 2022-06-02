ccacb7a8050ef19395006879ee804ce5680eda66
D O M Y <outdom.leap@esoftix.com>
2022-06-02 09:53:46 +0700

    updatr

---
+++ eAPI/eAPI/Migrations/20220602024911_sale_product_price_rule.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eAPI.Migrations
+{
+    public partial class sale_product_price_rule : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "price_rule_id",
+                table: "tbl_sale_product",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "price_rule_id",
+                table: "tbl_sale_product");
+        }
+    }
+}
