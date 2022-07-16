0775c82a608d404e94d759abecfb818f0ef23fb1
D O M Y <outdom.leap@esoftix.com>
2022-07-16 10:27:15 +0700

    update

---
+++ eAPIClient/eAPIClient/Migrations/20220716032324_sale_payment_payment_type.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eAPIClient.Migrations
+{
+    public partial class sale_payment_payment_type : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "payment_fee",
+                table: "tbl_sale_payment",
+                type: "decimal(19,8)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "payment_fee_amount",
+                table: "tbl_sale_payment",
+                type: "decimal(19,8)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "payment_fee",
+                table: "tbl_sale_payment");
+
+            migrationBuilder.DropColumn(
+                name: "payment_fee_amount",
+                table: "tbl_sale_payment");
+        }
+    }
+}
