45180a4b94edd8b14facdb837e80310db3302064
frappeerpnext <frapperpnext@gmail.com>
2023-02-04 11:48:27 +0700

    update requirement

---
+++ epos_frontend/src/components/ComInlineNote.vue
@@ -0,0 +1,195 @@
+<template>
+ 
+    <v-card>
+        <v-card-title>Note</v-card-title>
+        <v-card-text>
+            <div class="mb-2">
+                <div class="mb-2">
+                    <ComInput autofocus placeholder="Search or Add Note" keyboard v-model="search"
+                        v-debounce="onSearch" />
+                    <v-alert class="mt-4" v-if="getSelectedNote() != ''" :text="getSelectedNote()"></v-alert>
+                </div>
+                <div class="-m-1">
+                    <template v-for="(item, index) in getNote()" :key="index">
+                        <v-chip :closable="isDeleteNote" v-if="item.chip" @click:close="item.chip = false" class="m-1"
+                            @click="onSelected(item)">
+                            <v-icon start icon="mdi-checkbox-marked-circle-outline" v-if="item.selected"
+                                color="orange"></v-icon>
+                            <span>
+                                {{ item.note }}
+                            </span>
+                        </v-chip>
+                    </template>
+                </div>
+            </div>
+            <div>
+                <div class="text-right pt-4">
+                    <v-btn class="mr-2"  v-if="search" variant="flat" @click="onSaveNote" color="success">
+                            Save Note
+                    </v-btn>
+                    <template v-if="isDeleteNote">
+                        <v-btn class="mr-2" variant="flat" @click="onCancelDeleteNote" color="warning">
+                            Cancel
+                        </v-btn>
+                        <v-btn class="mr-2" v-if="noteResource.doc.notes.filter(r => r.chip == false).length > 0"
+                            variant="flat" @click="onDeleteNote" color="error">
+                            Delete
+                        </v-btn>
+
+                    </template>
+                    <template v-else>
+                        <v-btn class="mr-2" variant="flat" @click="onEnableDeleteNote" color="error">
+                            Delete Note
+                        </v-btn>
+
+                        <v-btn class="mr-2" variant="flat" @click="onClose(false)" color="error">
+                            Close
+                        </v-btn>
+
+                        <v-btn variant="flat" @click="onOK()" color="primary">
+                            OK
+                        </v-btn>
+                    </template>
+
+                </div>
+            </div>
+        </v-card-text>
+    </v-card>
+ 
+</template>
+<script setup>
+import { defineEmits, ref, createDocumentResource, confirmDialog, } from '@/plugin'
+import { createToaster } from '@meforma/vue-toaster';
+import Enumerable from 'linq'
+const emit = defineEmits(['resolve'])
+const props = defineProps({
+    params: Object
+})
+const toast = createToaster({position:"top"})
+const open = true;
+let search = ref()
+const isDeleteNote = ref(false);
+let selectedNotes = ref([]);
+
+const noteResource = createDocumentResource({
+    url: "frappe.client.get_list",
+    doctype: "Category Note",
+    name: props.params.name,
+    auto: true,
+    cache: ['category_note', props.params.name],
+    setValue: {
+    onSuccess() {
+toast.success("Success");
+
+    },
+  },
+    transform(doc) {
+        doc.notes.forEach(r => {
+            r.selected = false,
+                r.chip = true
+        })
+        return doc
+    }
+});
+
+
+function onSearch(keyword) {
+    search.value = keyword;
+}
+
+
+function getNote() {
+    if (search.value == undefined) search.value = "";
+    if (noteResource.doc !== null) {
+        let notes = noteResource.doc.notes;
+
+        if (props.params.data.product_code) {
+            notes =
+                notes.filter((r) => {
+                    return (r.product_code == props.params.data.product_code || r.product_code == '' || r.product_code == null)
+                })
+                ;
+        } else {
+ 
+            notes =
+                noteResource.doc?.notes.filter((r) => {
+                    return (!r.product_code || props.params.data.product_code)
+                })
+                ;
+        }
+        return notes.filter((r) => {
+            return String(r.note).toLocaleLowerCase().includes(search.value.toLocaleLowerCase());
+        });
+    }
+}
+function getSelectedNote() {
+    if (noteResource.doc == null) {
+        return "";
+    } else {
+        return Enumerable.from(noteResource.doc.notes).where("$.selected==true").select("$.note").toJoinedString(", ")
+    }
+}
+
+function onClose() {
+    emit('resolve', false)
+}
+function onOK() {
+    let selectedNote = getSelectedNote();
+    
+    if(!selectedNote){
+      
+        selectedNote = search.value;
+    }
+    if (selectedNote=="" || selectedNote==undefined ){
+        toast.warning("Please select or enter note");
+        return;
+    }
+
+
+    emit('resolve', selectedNote)
+
+}
+
+function onSelected(value) {
+    if (isDeleteNote.value == false) {
+        const selected = value.selected
+        if (noteResource.doc.multiple_selected == 0) {
+            Enumerable.from(noteResource.doc?.notes).where(`$.selected==true`).forEach("$.selected=false");
+        }
+        value.selected = !selected;
+    }
+
+}
+
+function onEnableDeleteNote() {
+    isDeleteNote.value = true;
+    noteResource.doc.notes.forEach((r) => {
+        r.selected = false;
+    })
+}
+function onCancelDeleteNote() {
+    isDeleteNote.value = false;
+    noteResource.doc.notes.forEach((r) => {
+        r.chip = true;
+    })
+
+
+}
+async function onDeleteNote() {
+    if (await confirmDialog({ title: "Delete Note", text: "Are you sure you want to delete note?" })) {
+        const notes = noteResource.doc.notes.filter(r => r.chip == true);
+        noteResource.doc.notes = notes
+
+        noteResource.setValue.submit({ notes: notes });
+        onCancelDeleteNote()
+    }
+
+}
+function onSaveNote(){
+
+    const notes = noteResource.doc.notes;
+    notes.push({note:search.value})
+    noteResource.setValue.submit({ notes: notes });
+   // search.value = "";
+}
+</script>
--- epos_frontend/src/views/sale/AddSale.vue
+++ epos_frontend/src/views/sale/AddSale.vue
@@ -15,8 +15,8 @@
                             <ComPlaceholder :is-not-empty="sale.getSaleProducts().length > 0" icon="mdi-cart-outline" text="Empty Product">
                                 <div v-for="(g, index) in sale.getSaleProductGroupByKey()" :key="index">
                                     <div class="bg-red-700 text-white flex items-center justify-between" style="font-size: 10px; padding: 2px;">
-                                        <div><v-icon icon="mdi-clock"></v-icon>{{  moment(g.order_time).format('HH:mm:ss') }}</div>
-                                        <div><v-icon icon="mdi-account-outline"></v-icon>{{g.order_by }}</div>
+                                        <div><v-icon icon="mdi-clock" size="small" class="mr-1"></v-icon>{{  moment(g.order_time).format('HH:mm:ss') }}</div>
+                                        <div><v-icon icon="mdi-account-outline" size="small" class="mr-1"></v-icon>{{g.order_by }}</div>
                                     </div>
                                     <v-list class="!p-0">
                                         <v-list-item v-for="sp,index in sale.getSaleProducts(g)" :key="index" @click="sale.onSelectSaleProduct(sp)"
