21b4e0b88fbd071ad262c46adbe7fc6381923422
frappeerpnext <frapperpnext@gmail.com>
2023-02-07 17:57:32 +0700

    update test page

--- epos_frontend/src/views/TestPage.vue
+++ epos_frontend/src/views/TestPage.vue
@@ -1,11 +1,14 @@
 <template>
-    
+    {{ platform }}
     <v-btn @click="changeX">Change Color</v-btn>
     
 </template>
 
 <script setup>
     import {ref} from "@/plugin"
+    import { useDisplay } from 'vuetify'
+ 
+const { mobile, name, platform } = useDisplay()
     function changeX(){
         alert("xxxxxxxxxxx");
         window.parent.postMessage('Hello from JS');
