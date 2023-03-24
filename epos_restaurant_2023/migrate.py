515431ef506555b7d2930d1ae0c5771a576afbb6
frappeerpnext <frapperpnext@gmail.com>
2023-03-24 18:50:02 +0700

    update print format

--- epos_restaurant_2023/migrate.py
+++ epos_restaurant_2023/migrate.py
@@ -1,3 +1,3 @@
 import frappe
 def after_migrate():
-    frappe.db.sql("delete from `tabPrint Format` where name = 'Close Sale Summary' and standard='Yes'")
+    frappe.db.sql("delete from `tabPrint Format` where name in ('Close Sale Summary','Close Sale Summary - UI') and standard='Yes'")
