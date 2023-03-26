e79127c6da19fa830c57b2fbc65f6aecfc3ddc7d
frappeerpnext <frapperpnext@gmail.com>
2023-03-26 17:57:17 +0700

    update unit of conversion

--- epos_restaurant_2023/migrate.py
+++ epos_restaurant_2023/migrate.py
@@ -1,3 +1,5 @@
 import frappe
 def after_migrate():
     frappe.db.sql("delete from `tabPrint Format` where name in ('Close Sale Summary','Close Sale Summary - UI') and standard='Yes'")
+    frappe.db.sql("delete from `tabUnit of Measurement Conversion` where name in ('e7c28b72f2')")
+    
