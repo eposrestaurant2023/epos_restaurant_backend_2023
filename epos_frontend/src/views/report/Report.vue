5d14098f8f82d961ef780d45ba0917daaf12af58
frappeerpnext <frapperpnext@gmail.com>
2023-04-04 10:29:51 +0700

    delete st_sf_print format

--- epos_frontend/src/views/report/Report.vue
+++ epos_frontend/src/views/report/Report.vue
@@ -2,9 +2,10 @@
     <PageLayout class="pb-4" :title="`${activeReport.doc_type}: ${activeReport.report_id}`" icon="mdi-chart-bar" full>
         <template #action>
             <v-btn icon="mdi-printer" @click="onPrint()"></v-btn>
+            <v-btn v-if="mobile" icon="mdi-filter-outline" @click="onOpenDrawer"></v-btn>
         </template>
         <v-row>
-            <v-col sm="4" md="4" lg="3">
+            <v-col sm="4" md="4" lg="3" v-if="!mobile">
                 <v-card>
                     <v-card-item>
                         <v-card-subtitle>
@@ -109,19 +110,6 @@
                                         </v-tab>
                                     </template>
                                     </v-tabs>
-                                    <!-- <div v-if="activeReport.name == 'Cashier Shift'">
-                                        <v-btn
-                                            v-for="(r, index) in gv.setting.reports.filter(r => r.doc_type == 'Cashier Shift' && r.show_in_pos == 1)"
-                                            :key="index" :color="activeReport.preview_report == r.name ? 'info' : 'default'"
-                                            class="m-1" @click="onPrintFormat(r)">{{ r.title }}</v-btn>
-                                    </div>
-                                    <div v-else-if="activeReport.name == 'Working Day'">
-                                        <v-btn
-                                            v-for="(r, index) in gv.setting.reports.filter(r => r.doc_type == 'Working Day' && r.show_in_pos == 1)"
-                                            :key="index" class="m-1"
-                                            :color="activeReport.preview_report == r.name ? 'info' : 'default'"
-                                            @click="onPrintFormat(r)">{{ r.title }}</v-btn>
-                                    </div> -->
                                 </v-col>
                                 <v-col cols="12" lg="5" xl="4">
                                     <div class="flex items-center justify-end">
@@ -139,13 +127,81 @@
                             </v-row>
                         </div>
                     </template>
-                    <v-card-text style="height: calc(100vh - 235px);">
-                        {{ printPreviewUrl }}
-                        <iframe id="report-view" height="100%" width="100%" :src="printPreviewUrl"></iframe>
+                    <v-card-text style="height: calc(100vh - 235px);"> 
+                        <ComPlaceholder :is-not-empty="activeReport.report_id != ''" :loading="workingDayReportsTmp.loading">
+                            <template #default>
+                                <iframe id="report-view" height="100%" width="100%" :src="printPreviewUrl"></iframe>
+                            </template>
+                        </ComPlaceholder>
                     </v-card-text>
                 </v-card>
             </v-col>
         </v-row>
+        <Sheet v-if="mobile" v-model:visible="drawer" onlyHeaderSwipe>
+            <div class="pa-4">
+                <ComPlaceholder :loading="workingDayReportsTmp.loading === true || workingDayReports === null"
+                                :is-not-empty="workingDayReports?.length > 0">
+                                <template v-for="(c, index) in workingDayReports" :key="index">
+                                    <v-card :color="activeReport.report_id == c.name ? 'info' : 'default'"
+                                        :variant="activeReport.report_id == c.name || c.cashier_shifts.find(r => r.name == activeReport.report_id) ? 'tonal' : 'text'"
+                                        class="bg-gray-200 my-2 subtitle-opacity-1" @click="onWorkingDay(c)">
+                                        <template v-slot:title>
+                                            <div class="flex justify-between">
+                                                <div>{{ c.name }}</div>
+                                                <div>
+                                                    <v-chip v-if="c.is_closed" color="error" size="small"
+                                                        variant="elevated">Closed</v-chip>
+                                                    <v-chip v-else color="success" size="small"
+                                                        variant="elevated">Opening</v-chip>
+                                                </div>
+                                            </div>
+                                        </template>
+                                        <template v-slot:subtitle>
+                                            <div class="whitespace-normal">
+                                                <div><v-icon icon="mdi-calendar" size="x-small" /> <span class="font-bold">{{
+                                                    c.posting_date
+                                                }}</span> opened by <span class="font-bold">{{ c.owner }}</span>
+                                                </div>
+                                                <div v-if="c.is_closed">
+                                                    <v-icon icon="mdi-calendar-multiple" size="x-small" /> <span
+                                                        class="font-bold">{{ c.closed_date }}</span> closed by <span
+                                                        class="font-bold">{{ c.modified_by }}</span>
+                                                </div>
+                                                <div><v-icon icon="mdi-note-text" size="x-small"></v-icon> Total Shift: <span
+                                                        class="font-bold">{{ c.cashier_shifts.length }}</span></div>
+                                            </div>
+                                        </template>
+                                    </v-card>
+                                    <div class="overflow-y-auto overflow-x-hidden max-h-44"
+                                        v-if="activeReport.report_id == c.name || c.cashier_shifts.find(r => r.name == activeReport.report_id)">
+                                        <div class="flex flex-wrap">
+                                            <v-sheet 
+                                                elevation="1" 
+                                                :color="item.name == activeReport.report_id ? 'info' : 'default'" 
+                                                class="m-1 p-2 text-center cursor-pointer"
+                                                width="118px"
+                                                rounded="sm"
+                                                v-for="(item, index) in c.cashier_shifts"
+                                                :key="index" @click="onCashierShift(item)"> 
+                                                
+                                                <div>{{ moment(item.creation).format('h:mm:ss A') }}</div>
+                                                <div class="text-xs">
+                                                    <span>#{{ item.name }}</span>
+                                                </div>
+                                                <div>
+                                                    <v-chip v-if="item.is_closed" size="x-small" color="error">Closed</v-chip>
+                                                    <v-chip v-else size="x-small" color="success">Opening</v-chip>
+                                                </div>
+                                            </v-sheet>
+                                        </div>
+                                    </div>
+                                    <div class="pt-2">
+                                        <hr />
+                                    </div>
+                                </template>
+                            </ComPlaceholder>
+            </div>
+        </Sheet>
     </PageLayout>
 </template>
 <script setup>
@@ -153,11 +209,15 @@ import { createResource, inject, computed, ref, saleDetailDialog, onUnmounted } 
 import Enumerable from 'linq'
 import { webserver_port } from "../../../../../../sites/common_site_config.json"
 import PageLayout from '@/components/layout/PageLayout.vue';
-import { createToaster } from '@meforma/vue-toaster';
+import { Sheet } from 'bottom-sheet-vue3'
+import ComPlaceholder from '../../components/layout/components/ComPlaceholder.vue';
+import {useDisplay} from 'vuetify'
+const {mobile} = useDisplay()
 const gv = inject('$gv')
 const moment = inject('$moment')
 const selectedLetterhead = ref(getDefaultLetterHead());
 const printPreviewUrl = ref("");
+const drawer = ref(false)
 const serverUrl = window.location.protocol + "//" + window.location.hostname + ":" + webserver_port;
 
 const activeReport = ref({
@@ -212,7 +272,9 @@ const workingDayReportsTmp = createResource({
     }
 })
 
-
+function onOpenDrawer() {
+    drawer.value = true
+}
 
 
 function onCashierShift(data) {
