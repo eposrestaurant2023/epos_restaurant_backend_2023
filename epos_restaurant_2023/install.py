2e8e7aedf1604131cb81b140689fb4cda402c9ce
frappeerpnext <frapperpnext@gmail.com>
2023-04-05 09:12:23 +0700

    msgprint create predefine data

--- epos_restaurant_2023/install.py
+++ epos_restaurant_2023/install.py
@@ -259,7 +259,7 @@ def create_predefine_data():
     if frappe.session.user == 'Administrator':
         data = frappe.db.get_list('Predefine Data',fields=["*"], order_by='sort asc')     
         for d in data:   
-            frappe.msgprint(d.doc_type + " | " + d.doc_name)
+            frappe.msgprint(d.doc_type + " | " + d.doc_name + "\n")
             if d.is_single == 0 :   
                 if not frappe.db.exists(d.doc_type, d.doc_name): 
                     doc = frappe.get_doc(json.loads(d.data))
