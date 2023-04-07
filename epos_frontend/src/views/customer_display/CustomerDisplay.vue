92a17434d1587de1018ef4fa4f36a848a7bdbec7
frappeerpnext <frapperpnext@gmail.com>
2023-04-07 15:39:07 +0700

    update

--- epos_frontend/src/views/customer_display/CustomerDisplay.vue
+++ epos_frontend/src/views/customer_display/CustomerDisplay.vue
@@ -1,16 +1,17 @@
 <template>
-    
-
 <ul class="bg-violet-950">
     <li v-for="(p, index) in sale.sale_products" :key="index">
         {{ p.product_code }} - {{ p.product_name }}
     </li>
 </ul>
-{{ sale.sale_products }}
+{{ sale }}
 </template>
 <script setup>
     import { inject } from '@/plugin';
     import { ref } from 'vue';
+
+    const data = ref()
+
     const socket = inject("$socket")
     const gv = inject("$gv")
     const sale = ref({})
