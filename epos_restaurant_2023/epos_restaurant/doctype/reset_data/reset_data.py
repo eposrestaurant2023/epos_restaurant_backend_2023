90ba91dce60e2b95523890c2dd9f16885a0df30e
frappeerpnext <frapperpnext@gmail.com>
2023-04-20 08:37:12 +0700

    update

--- epos_restaurant_2023/epos_restaurant/doctype/reset_data/reset_data.py
+++ epos_restaurant_2023/epos_restaurant/doctype/reset_data/reset_data.py
@@ -9,22 +9,10 @@ from passlib.hash import pbkdf2_sha256
 class ResetData(Document):
 	def validate(self):
 		if self.password is None or self.password == "":
-			frappe.throw("shit")
+			frappe.throw("Please Enter Password")
 		else:
-			users = frappe.db.sql("""select name,password from __Auth where doctype='User' and fieldname='password' and encrypted = 0""",as_dict=True)
-			for user in users:
-				
-				if pbkdf2_sha256.verify(self.password, user.password):
-					frappe.msgprint(user.name)
-					user_role_profile = frappe.db.get_list('User',filters={'name': user.name},fields=['role_profile_name'],as_list=True)
-					if len(user_role_profile) > 0:
-						role_profile = frappe.db.get_list('Role Profile',filters={'name': user_role_profile[0].role_profile_name},fields=['role_profile'],as_list=True)
-						if len(role_profile) > 0:
-							roles = frappe.db.get_list('Has Role',filters={'parent': role_profile[0].role_profile},fields=['role'],as_list=True)
-							if len(roles) > 0:
-								if "System Manager" in roles:
-									frappe.msgprint("yes")
-								else:
-									frappe.msgprint("no")
-				
-			
+			user = frappe.db.sql("select password from __Auth where name ='Administrator' and doctype='User' and fieldname='password'",as_dict=True)
+			if pbkdf2_sha256.verify(self.password, user[0].password):
+				frappe.msgprint("")
+			else:
+				frappe.throw("Wrong Password")
