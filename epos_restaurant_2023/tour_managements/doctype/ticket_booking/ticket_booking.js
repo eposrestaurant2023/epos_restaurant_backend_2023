d604488b661d3497ff1d20e84f0ac3aa7db4833f
frappeerpnext <frapperpnext@gmail.com>
2023-04-23 17:53:46 +0700

    update ticket booking

--- epos_restaurant_2023/tour_managements/doctype/ticket_booking/ticket_booking.js
+++ epos_restaurant_2023/tour_managements/doctype/ticket_booking/ticket_booking.js
@@ -1,8 +1,19 @@
 // Copyright (c) 2023, Tes Pheakdey and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Ticket Booking", {
-// 	refresh(frm) {
+frappe.ui.form.on("Ticket Booking", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+    ticket_type(frm){
+        alert(frm.doc.ticket_type)
+        frm.set_query("ticket_code","ticket_booking_item", function() {
+            return {
+                filters: [
+                    ["Ticket","ticket_category", "=", frm.doc.ticket_type]
+                ]
+            }
+        });
+    }
+});
+
