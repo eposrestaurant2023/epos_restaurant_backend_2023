ba3e3c32a9d8c5c1270f4149bed3a6e9dfe3ca4a
frappeerpnext <frapperpnext@gmail.com>
2023-04-23 13:52:36 +0700

    update tour package price

--- epos_restaurant_2023/tour_managements/doctype/tour_packages/tour_packages.py
+++ epos_restaurant_2023/tour_managements/doctype/tour_packages/tour_packages.py
@@ -8,3 +8,29 @@ from frappe.model.document import Document
 class TourPackages(Document):
 	def validate(self):
 		self.total_cost =   Enumerable(self.cost_breakdowns).sum(lambda x: (x.rate or 0))
+		self.total_fix_cost=   Enumerable(self.fix_cost).sum(lambda x: (x.rate or 0))
+
+		if self.total_cost:
+			for d in self.tour_package_prices:
+				d.fix_cost = self.total_fix_cost
+				transportation_cost = get_transportation_cost(self,d.number_of_person)
+				d.transportation_type = transportation_cost["transportation_type"]
+				d.transportation_cost = transportation_cost["cost"] / (d.number_of_person or 1)
+				d.additional_cost = ( self.total_cost / (d.number_of_person or 1))
+				d.price = self.total_fix_cost + d.transportation_cost  + d.additional_cost 
+
+
+def get_transportation_cost(self,pax):
+	
+	for d in self.transportation_cost:
+		
+		if pax in range(d.min_pax,d.max_pax+1):
+			
+			return {
+				"transportation_type":d.transportation_type,
+				"cost":d.rate or 0
+			}
+	return {
+				"transportation_type":"",
+				"cost":0
+			}
