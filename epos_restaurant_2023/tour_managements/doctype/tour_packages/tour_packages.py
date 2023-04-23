18291d70cca1eaa9a510b91adf394aba9cf8eae1
frappeerpnext <frapperpnext@gmail.com>
2023-04-23 11:14:53 +0700

    update tour package

--- epos_restaurant_2023/tour_managements/doctype/tour_packages/tour_packages.py
+++ epos_restaurant_2023/tour_managements/doctype/tour_packages/tour_packages.py
@@ -1,8 +1,10 @@
 # Copyright (c) 2023, Tes Pheakdey and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from py_linq import Enumerable
 from frappe.model.document import Document
 
 class TourPackages(Document):
-	pass
+	def validate(self):
+		self.total_cost =   Enumerable(self.cost_breakdowns).sum(lambda x: (x.rate or 0))
