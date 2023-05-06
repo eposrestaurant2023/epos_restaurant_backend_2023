839fa73d7a49c3ae8b765eede8245093856408c6
frappeerpnext <frapperpnext@gmail.com>
2023-05-06 14:11:56 +0700

    update payment type

--- epos_restaurant_2023/epos_restaurant/doctype/payment_type/payment_type.py
+++ epos_restaurant_2023/epos_restaurant/doctype/payment_type/payment_type.py
@@ -6,14 +6,18 @@ from frappe.model.document import Document
 
 class PaymentType(Document):
 	def validate(self):
-		 
-		data = frappe.db.sql("select exchange_rate from `tabCurrency Exchange` where to_currency='{}' order by posting_date desc limit 1".format(self.currency))
-		if data:
-			 
-			self.exchange_rate = data[0][0] or 1
-		else:
-			self.exchange_rate  =1 
-	
+
+		if self.currency == frappe.db.get_default("currency"):
+			self.exchange_rate = 1
+		else: 
+			data = frappe.db.sql("select exchange_rate from `tabCurrency Exchange` where to_currency='{}' order by posting_date desc limit 1".format(self.currency))
+			
+			if data:
+
+				self.exchange_rate = data[0][0] or 1
+			else:
+				self.exchange_rate  =1 
+		
 	def on_update(self):
 		if self.has_value_changed("payment_type_group"):
 			frappe.db.sql("update `tabSale Payment` set payment_type_group='{}' where payment_type='{}'".format(self.payment_type_group,self.name))
