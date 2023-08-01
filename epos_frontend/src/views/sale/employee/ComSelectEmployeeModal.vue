6c4f64a2eeb212b87992e78588a59a7fadcab77a
frappeerpnext <frapperpnext@gmail.com>
2023-08-01 15:18:24 +0700

    update pos

--- epos_frontend/src/views/sale/employee/ComSelectEmployeeModal.vue
+++ epos_frontend/src/views/sale/employee/ComSelectEmployeeModal.vue
@@ -5,73 +5,110 @@
             {{ $t('Employee') }}
         </template>
         <template #content>
-            <div class="search-box my-0 mx-auto" :class="small ? 'w-full' : 'max-w-[350px]'">
-            <ComInput
-                autofocus
-                keyboard
-                variant="outlined"
-                :placeholder="$t('Search...')"
-                v-model="keyword"
-                prepend-inner-icon="mdi-magnify"
-                v-debounce="getEmployessList"
-                :ref="control"
-                />
-            </div>
+            <div class="container mx-auto grid grid-cols-3 gap-4 mt-4" :class="mobile ? 'grid-cols-1' : 'sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3'">
+                <div class="box-panel-custom p-4">
+                    <label>Employee</label>
+                    <!-- <div class="search-box my-0 mx-auto" :class="small ? 'w-full' : 'max-w-[350px]'"> -->
+                    <div class="search-box my-0 mx-auto w-full py-2">
+                    <ComInput
+                        autofocus
+                        keyboard
+                        variant="outlined"
+                        :placeholder="$t('Search...')"
+                        v-model="keyword"
+                        prepend-inner-icon="mdi-magnify"
+                        v-debounce="getEmployessList"
+                        :ref="control"
+                        />
+                    </div>
 
-            <div class="grid gap-2" :class="mobile ? 'grid-cols-2' : 'sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-6'">          
-                <div v-for="(e, index) in getEmployessList(keyword)" :key="index" class="h-10">
-                    <div class="h-full rounded-lg shadow-lg cursor-pointer " :class="e.is_selected?'bg-red':'bg-gray-500'">
-                        <div v-ripple class="relative p-2 w-full h-full flex justify-center items-center" @click="onEmployeeSelected(e)">
-                            <div class="text-white">{{`${e.name} - ${e.employee_name}` }}</div>
+                    <div class="grid grid-cols-2 gap-2" :class="mobile ? 'grid-cols-2' : 'sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2'">          
+                        <div v-for="(e, index) in getEmployessList(keyword)" :key="index">
+                            <div class="h-full rounded-lg shadow-lg cursor-pointer border" :class="e.is_selected?'bg-red':'bg-white'">
+                                <div v-ripple class="relative p-2 w-full h-full inline-flex" @click="onEmployeeSelected(e)">
+                                    <span class="pr-3">
+                                        <img class="rounded-full border" :src="avatarProfile"/>
+                                    </span>
+                                    <span class="flex items-center">
+                                        <div>
+                                            {{`${e.name} - ${e.employee_name}` }}<br/>
+                                            <small>Therapies</small>
+                                        </div>
+                                        
+                                    </span>
+                                </div>
+                            </div>
+                        </div> 
+                    </div>
+                </div>
+
+                <div class="box-panel-custom p-4">
+                    <div class="box-panel-custom p-4 rounded">
+                        <label>Duration</label>
+                        <div class="w-full py-2">
+                            <v-btn @click="onDurationTypePressed()" class="rounded-t-lg" :class="!is_overtime?'bg-red':'bg-gray-500'">
+                                {{$t('General')}}
+                            </v-btn>
+                            <v-btn @click="onDurationTypePressed(true)" class="rounded-t-lg" :class="is_overtime?'bg-red':'bg-gray-500'">
+                                {{$t('Overtime')}}
+                            </v-btn>
+                            <hr/>
+                        </div>
+                    
+                        <div class="grid gap-2 mt-4" :class="mobile ? 'grid-cols-2' : 'sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-6 2xl:grid-cols-3'">          
+                            <div v-for="(e, index) in get_duration_commission_list" :key="index" class="h-10">
+                                <div class="h-full rounded-lg shadow-lg cursor-pointer " :class="e.is_selected?'bg-red':'bg-gray-500'">
+                                    <div v-ripple class="relative p-2 w-full h-full flex justify-center items-center" @click="onDurationSelected(e)">
+                                        <div class="text-white">{{`${e.duration_title}${e.is_overtime?'-OT':''}` }}</div>
+                                    </div>
+                                </div>
+                            </div> 
                         </div>
                     </div>
-                </div> 
-            </div>
 
-            <hr/>
-            <div>Duration</div>
-            <v-btn @click="onDurationTypePressed()" :class="!is_overtime?'bg-red':'bg-gray-500'">
-                {{$t('General')}}
-              </v-btn>
-              <v-btn @click="onDurationTypePressed(true)" :class="is_overtime?'bg-red':'bg-gray-500'">
-                {{$t('Overtime')}}
-              </v-btn>
-
-            <div class="grid gap-2" :class="mobile ? 'grid-cols-2' : 'sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-6'">          
-                <div v-for="(e, index) in get_duration_commission_list" :key="index" class="h-10">
-                    <div class="h-full rounded-lg shadow-lg cursor-pointer " :class="e.is_selected?'bg-red':'bg-gray-500'">
-                        <div v-ripple class="relative p-2 w-full h-full flex justify-center items-center" @click="onDurationSelected(e)">
-                            <div class="text-white">{{`${e.duration_title}${e.is_overtime?'-OT':''}` }}</div>
+                    <div class="box-panel-custom p-4 mt-4 rounded">
+                        <div>Commission</div>
+                        <div class="grid gap-2 py-2" :class="mobile ? 'grid-cols-2' : 'sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-6'">          
+                            <div v-for="(e, index) in get_commission_list" :key="index" class="h-10">
+                                <div class="h-full rounded-lg shadow-lg cursor-pointer " :class="e.is_selected?'bg-red':'bg-gray-500'">
+                                    <div v-ripple class="relative p-2 w-full h-full flex justify-center items-center" @click="onCommissionSelected(e)">
+                                        <div class="text-white">{{`${e.commission_value}` }}</div>
+                                    </div>
+                                </div>
+                            </div> 
                         </div>
                     </div>
-                </div> 
+                </div>
+
+
+                <div class="box-panel-custom p-4">
+                    <div>Selected Employees</div>
+                    <ul v-for="(e, index) in employees_selected" :key="index">
+                        <li class="w-full py-2">
+                            <div class="inline-flex box-panel-custom w-full p-4">
+                                <div class="pr-3">
+                                    <img class="rounded-full border" :src="avatarProfile" width="100"/>
+                                </div>
+                                <div class="w-full">
+                                    <div class="flex justify-between">
+                                        <div>
+                                            <span>{{e.employee_name }}</span><br/>
+                                            <small>Therapy</small><br/>
+                                            <small><v-icon icon="md:home"></v-icon>{{e.duration_title}}</small>
+                                        </div>
+                                        <div>
+                                            <strong>${{e.commission_amount}}</strong>
+                                        </div>
+                                    </div>
+                                    <v-btn @click="onRemoveSelectedEmployee(e)" class="w-full bg-red dl">
+                                        {{$t('Delete')}}
+                                    </v-btn>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
-
-
-            <hr/>
-            <div>Commission</div>
-            <div class="grid gap-2" :class="mobile ? 'grid-cols-2' : 'sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-6'">          
-                <div v-for="(e, index) in get_commission_list" :key="index" class="h-10">
-                    <div class="h-full rounded-lg shadow-lg cursor-pointer " :class="e.is_selected?'bg-red':'bg-gray-500'">
-                        <div v-ripple class="relative p-2 w-full h-full flex justify-center items-center" @click="onCommissionSelected(e)">
-                            <div class="text-white">{{`${e.commission_value}` }}</div>
-                        </div>
-                    </div>
-                </div> 
-            </div>
-
-
-            <hr/>
-
-           <div>Selected Employees</div>
-            <ul  v-for="(e, index) in employees_selected" :key="index">
-                <li>
-                    {{e.employee_name }}({{e.duration_title}})
-                    <v-btn @click="onRemoveSelectedEmployee(e)">
-                        {{$t('Delete')}}
-                      </v-btn>
-                </li>
-            </ul>
 
 
         </template>
@@ -93,6 +130,8 @@ import { useDisplay } from 'vuetify'
 import { createToaster } from '@meforma/vue-toaster';
 import ComInput from '../../../components/form/ComInput.vue';
 import { computed } from 'vue';
+
+import avatarProfile from '@/assets/svg/user-icon-profile.svg';
 
 const frappe = inject("$frappe")
 const db = frappe.db();
@@ -262,3 +301,17 @@ function onClose(){
 
  
 </script>
+
+<style>
+.box-panel-custom{
+    box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
+}
+.box-panel-custom .v-btn:not(.dl){
+    border-bottom-left-radius: 0 !important;
+    border-bottom-right-radius: 0 !important;
+}
+.box-panel-custom .v-btn--variant-elevated{
+    box-shadow: unset !important;
+    background-color: rgb(228 228 231);
+}
+</style>
