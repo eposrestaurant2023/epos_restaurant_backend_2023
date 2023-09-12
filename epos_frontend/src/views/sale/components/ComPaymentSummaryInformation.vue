8d6468f707495ff3cb734b3e1a05af5a8b154c1e
outdomleap <outdom.leap@esoftix.com>
2023-09-12 14:25:45 +0700

    update

--- epos_frontend/src/views/sale/components/ComPaymentSummaryInformation.vue
+++ epos_frontend/src/views/sale/components/ComPaymentSummaryInformation.vue
@@ -1,6 +1,14 @@
 <template> 
     <div v-if="sale.sale.total_paid > 0 || balance > 0 || sale.sale.changed_amount > 0" class="mt-auto bg-green-600 border border-gray-500 text-white rounded-sm px-1 pt-1" :class="mobile ? 'mx-1' : ''">
-        
+  
+        <div class="mb-1 flex justify-between" v-if="sale.sale.tip_amount > 0">
+            <div>{{ $t('TIP') }}:</div>
+            <div>
+                <CurrencyFormat :value="(sale.sale.tip_amount)" />
+            </div>
+        </div>
+        <hr v-if="(sale.sale.tip_amount ||0) > 0"/>
+
         <div class="mb-1 flex justify-between text-sm" v-if="(sale.sale.total_fee ||0) > 0">
             <div>{{ $t('Fee') }}:</div>
             <div>
